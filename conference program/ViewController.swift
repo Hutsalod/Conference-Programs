b69ea568ecc423c45441175b32eb27ab0428f097
Hutsalod <hutsalod@gmail.com>
2020-02-20 17:55:02 +0200

    Initial Commit

---
+++ conference program/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  conference program
+//
+//  Created by Mac Book  on 20.02.2020.
+//  Copyright © 2020 Mac Book . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
